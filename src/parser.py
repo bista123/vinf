1fb86ea22e53fd65f5bbd0d526953a0d5eaeca1b
Jakub Bista <jakub.bista@gmail.com>
2020-10-17 12:20:57 +0200

    Update encoding

--- src/parser.py
+++ src/parser.py
@@ -4,8 +4,8 @@ import codecs
 
 def parse_ids():
 
-    ttl_file = codecs.open("D:/Jakub/FIIT/ING/2/VINF/vinf_project/vinf/data/page_ids_sample.ttl", encoding='utf-8')
-    csv_file = open("D:/Jakub/FIIT/ING/2/VINF/vinf_project/vinf/data/page_ids.csv", "w")
+    ttl_file = codecs.open('D:/Jakub/FIIT/ING/2/VINF/vinf_project/vinf/data/page_ids_sample.ttl', 'r', encoding='utf-8')
+    csv_file = codecs.open('D:/Jakub/FIIT/ING/2/VINF/vinf_project/vinf/data/page_ids.csv', 'w', encoding='utf-8')
 
     lines = ttl_file.readlines()
 
@@ -23,8 +23,8 @@ def parse_ids():
 
 def parse_labels():
 
-    ttl_file = codecs.open("D:/Jakub/FIIT/ING/2/VINF/vinf_project/vinf/data/labels_sample.ttl", encoding='utf-8')
-    csv_file = open("D:/Jakub/FIIT/ING/2/VINF/vinf_project/vinf/data/labels.csv", "w")
+    ttl_file = codecs.open('D:/Jakub/FIIT/ING/2/VINF/vinf_project/vinf/data/labels_sample.ttl', 'r', encoding='utf-8')
+    csv_file = codecs.open('D:/Jakub/FIIT/ING/2/VINF/vinf_project/vinf/data/labels.csv', 'w', encoding='utf-8')
 
     lines = ttl_file.readlines()
 
@@ -42,7 +42,9 @@ def parse_labels():
 
 def parse_categories():
 
-    ttl_file = codecs.open('D:/Jakub/FIIT/ING/2/VINF/vinf_project/vinf/data/article_categories_sample.ttl', encoding='utf-8')
+    ttl_file = codecs.open('D:/Jakub/FIIT/ING/2/VINF/vinf_project/vinf/data/article_categories_sample.ttl',
+                           'r',
+                           encoding='utf-8')
 
     lines = ttl_file.readlines()
     categories = {}
@@ -60,7 +62,9 @@ def parse_categories():
         except:
             continue
 
-    csv_file = open("D:/Jakub/FIIT/ING/2/VINF/vinf_project/vinf/data/article_categories.csv", "w")
+    csv_file = codecs.open('D:/Jakub/FIIT/ING/2/VINF/vinf_project/vinf/data/article_categories.csv',
+                           'w',
+                           encoding='utf-8')
 
     for label, category in categories.items():
         tostr = ' '.join(map(str, category))
@@ -72,7 +76,9 @@ def parse_categories():
 
 def parse_links():
 
-    ttl_file = codecs.open('D:/Jakub/FIIT/ING/2/VINF/vinf_project/vinf/data/page_links_sample.ttl', encoding='utf-8')
+    ttl_file = codecs.open('D:/Jakub/FIIT/ING/2/VINF/vinf_project/vinf/data/page_links_sample.ttl',
+                           'r',
+                           encoding='utf-8')
 
     lines = ttl_file.readlines()
     links = {}
@@ -90,7 +96,7 @@ def parse_links():
         except:
             continue
 
-    csv_file = open("D:/Jakub/FIIT/ING/2/VINF/vinf_project/vinf/data/page_links.csv", "w")
+    csv_file = codecs.open('D:/Jakub/FIIT/ING/2/VINF/vinf_project/vinf/data/page_links.csv', 'w', encoding='utf-8')
 
     for label, link in links.items():
         tostr = ' '.join(map(str, link))
@@ -108,7 +114,7 @@ def merge_together():
 
     for file, label in zip(files, labels):
 
-        parse_file = open(f"D:/Jakub/FIIT/ING/2/VINF/vinf_project/vinf/data/{file}", "r")
+        parse_file = codecs.open(f'D:/Jakub/FIIT/ING/2/VINF/vinf_project/vinf/data/{file}', 'r', encoding='utf-8')
         lines = parse_file.readlines()
 
         for line in lines:
@@ -127,7 +133,9 @@ def merge_together():
 
         parse_file.close()
 
-    output_file = open("D:/Jakub/FIIT/ING/2/VINF/vinf_project/vinf/data/dictionaries.csv", "w")
+    output_file = codecs.open('D:/Jakub/FIIT/ING/2/VINF/vinf_project/vinf/data/dictionaries.csv',
+                              'w',
+                              encoding='utf-8')
 
     for upper_label, lower_label in output.items():
         output_file.write(upper_label + ":" + "\n")
